a6891efab45bac487a1e025180d8be41e25af9f4
janethu8 <janethu8@stanford.edu>
2018-03-10 14:24:32 -0800

    updated folder names

--- meal-recommender/wsgi.py
+++ meal-recommender/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "basicform.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "athleteForm.settings")
 
 application = get_wsgi_application()
